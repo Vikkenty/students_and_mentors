c9af1ebb6b32c360429cad5ecc3eefd5ed2d276c
Viktory <victoriyayakovleva85@gmail.com>
2023-07-04 00:11:02 +0300

    Классный журнал.py changed

--- Классный журнал.py
+++ Классный журнал.py
@@ -9,14 +9,31 @@ class Student:
  
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)   
- 
+
+    def rate_hw(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in lecturer.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка' 
      
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
-        
+                
+
+class Lecturer (Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.courses_in_progress = []
+        self.grades = {}
+    
+
+class Reviewer (Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -26,7 +43,6 @@ class Mentor:
         else:
             return 'Ошибка'
         
-
 
  
 best_student = Student('Ruoy', 'Eman', 'your_gender')
